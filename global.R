fecb0c74c15ddeb9f3f81dc0455247a193bf4bb2
pkalanta <parvin.kalantari@nrcan-rncan.gc.ca>
2025-08-06 15:16:14 -0700

    project updates

--- global.R
+++ global.R
@@ -1,31 +1,56 @@
 
+rm(list = ls())  # clears all objects
+gc()             # clears memory
+
+
 repos <- c("https://predictiveecology.r-universe.dev", getOption("repos"))
 if (!require("SpaDES.project")) {
-  install.packages("SpaDES.project", repos = repos)
+  install.packages(c("SpaDES.project", "Require"), repos = repos)
 }
 
 out <- SpaDES.project::setupProject(
   paths = list(
-    projectPath = getwd()
+    projectPath = getwd(), 
+    inputPath = file.path("inputs"),
+    output = file.path("outputs"), 
+    cachePath = file.path("cache"),
+    modulePath = file.path("modules")
   ),
   useGit = TRUE,
   restart = TRUE,
-  modules = c("PredictiveEcology/Biomass_core@development", 
-              "ianmseddy/simpleHarvest@development"),
+  modules = c(
+    "PredictiveEcology/Biomass_borealDataPrep@development",
+    "PredictiveEcology/Biomass_core@development",
+    "ianmseddy/LandR_reforestation@development",
+    #scfm,
+    "PredictiveEcology/scfm@development",
+    "PredictiveEcology/Biomass_regeneration@development",
+    "pkalanta/simpleHarvest@parvintesting"),
   packages = c(
     'RCurl', 'XML', 'snow', 'googledrive', 
-    'httr2', "gert", "remotes"
+    'httr2', "gert", "remotes", "terra","data.table"
   ),
-  require = c("PredictiveEcology/LandR"),
-  times = list(start = 2011, end = 2012),
+  
+  require = c("PredictiveEcology/LandR@development (>= 1.1.5.9048)",
+              "PredictiveEcology/SpaDES.core@box"),
+  times = list(start = 2011, end = 2021),
   options = options(reproducible.useMemoise = TRUE),
   params = list(
     globals = list(
-      "sppEquivCol" = "LandR"
-    )
+      "sppEquivCol" = "LandR", 
+      ".plots" = "png",
+      cohortDefinitionCols = c("speciesCode", "age", "foo")
+    ), 
+    simpleHarvest = list(.useCache = ".inputObjects")
   ),
   studyArea = {
-   sa <- LandR::randomStudyArea(size = 10000 * 6.25 * 15000, seed = 99)
+    sa <- prepInputs(url = "https://sis.agr.gc.ca/cansis/nsdb/ecostrat/region/ecoregion_shp.zip", 
+                     destinationPath = "inputs")
+    sa <- sa[sa$REGION_NAM == "Thompson-Okanagan Plateau",]
+    sa <- sf::st_transform(sa, sf::st_crs(paste("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0", 
+                                              "+datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")))
+    sa <- sf::st_buffer(sa, 5000)
+    sa <- terra::vect(sa)
   },
   sppEquiv = {
     spp <- LandR::speciesInStudyArea(studyArea = sa, dPath = "inputs", sppEquivCol = "LandR")
@@ -34,8 +59,18 @@ out <- SpaDES.project::setupProject(
     sppEquiv <- sppEquiv[!LANDIS_traits == "",]
   },
   rasterToMatch = {
-  rtm <- terra::rast(studyArea, vals = 1, res = c(250, 250)) |>
-  terra::mask(mask = studyArea)
+    rtm <- terra::rast(studyArea, vals = 1, res = c(250, 250)) |>
+      terra::mask(mask = studyArea)
   }
 )
+
+#annoying steps because scfm is annoying:
+out$paths$modulePath <- c("modules", "modules/scfm/modules")
+out$modules <- setdiff(c(out$modules, 
+                          c("scfmDataPrep", "scfmIgnition", "scfmEscape", "scfmSpread")), 
+                          "scfm")
+out$params$scfmDataPrep$targetN <- 1000 #quick calibration while testing
+
+outSim <- do.call(SpaDES.core::simInitAndSpades, out)
+
 
