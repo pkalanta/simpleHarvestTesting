4f7cfdec7a16414f46a955d8ce7e5cc6944439b6
pkalanta <parvin.kalantari@nrcan-rncan.gc.ca>
2025-09-08 15:06:47 -0700

    Update global.R

--- global.R
+++ global.R
@@ -10,51 +10,56 @@ if (!require("SpaDES.project")) {
 
 out <- SpaDES.project::setupProject(
   paths = list(
-    projectPath = getwd(), 
+    projectPath = getwd(),
     inputPath = file.path("inputs"),
-    outputPath = file.path("outputs"), 
+    outputPath = file.path("outputs"),
     cachePath = file.path("cache"),
     modulePath = file.path("modules")
   ),
-  useGit = TRUE,
+
+  # useGit = TRUE,
   restart = TRUE,
   modules = c(
     "PredictiveEcology/Biomass_borealDataPrep@development",
     "PredictiveEcology/Biomass_core@development",
     "ianmseddy/LandR_reforestation@parvindev",
-    #scfm,
     "PredictiveEcology/scfm@development",
-    "PredictiveEcology/Biomass_regeneration@development",
-    #"pkalanta/simpleHarvestTesting@development"
-    "pkalanta/simpleHarvest@parvintesting"
-    ),
+    "PredictiveEcology/Biomass_regeneration@development"
+    , "pkalanta/simpleHarvest@parvintesting"
+  ),
+
   packages = c(
     'RCurl', 'XML', 'snow', 'googledrive', 
-    'httr2', "gert", "remotes", "terra","data.table"
+    'httr2', "gert", "remotes", "terra", "data.table"
   ),
   
-  require = c("PredictiveEcology/LandR@development", 
-              "PredictiveEcology/SpaDES.core@box"),
+  require = c(
+    "PredictiveEcology/LandR@development",
+    "PredictiveEcology/SpaDES.core@box"
+  ),
   
   times = list(start = 2011, end = 2021),
-  options = options(reproducible.useMemoise = TRUE),
+  # options = options(reproducible.useMemoise = TRUE),
+  options = list(reproducible.useMemoise = TRUE,
+                 spades.moduleCodeChecks = FALSE,
+                 spades.allowInitDuringSimInit = TRUE),
   params = list(
     globals = list(
       sppEquivCol = "LandR", 
       .plots = "png",
       .plotInterval = 1,
-      .useCache = "none",
+      .useCache = c(".inputObjects", "init"),
       cohortDefinitionCols = c("speciesCode", "age", "foo")
     ), 
-    simpleHarvest = list(
-      .useCache = ".inputObjects",
-      blockTargets = c("block1" = 0.1, "block2" = 0.05) 
-      ),
+    simpleHarvest = list(.useCache = ".inputObjects"),
     Biomass_core = list(.plots = NA)
   ),
+  
   studyArea = {
-    sa <- prepInputs(url = "https://sis.agr.gc.ca/cansis/nsdb/ecostrat/region/ecoregion_shp.zip", 
-                     destinationPath = "inputs")
+    sa <- prepInputs(
+           url = "https://sis.agr.gc.ca/cansis/nsdb/ecostrat/region/ecoregion_shp.zip", 
+           destinationPath = "inputs"
+           )
     sa <- sa[sa$REGION_NAM == "Thompson-Okanagan Plateau",]
     sa <- sf::st_transform(sa, sf::st_crs(paste("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0", 
                                                 "+datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")))
@@ -69,7 +74,7 @@ out <- SpaDES.project::setupProject(
   },
   rasterToMatch = {
     rtm <- terra::rast(studyArea, vals = 1, res = c(250, 250)) |>
-      terra::mask(mask = studyArea)
+    terra::mask(mask = studyArea)
   }
 )
 
@@ -82,5 +87,6 @@ out$modules <- setdiff(c(out$modules,
 out$params$scfmDataPrep$targetN <- 1000 #quick calibration while testing
 
 outSim <- do.call(SpaDES.core::simInitAndSpades, out)
+
 
 
